51fe05282fc43cc62dcd6cdbce2974230af5b2da
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-10-10 15:16:02 -0700

    Created Pop method that grabs the last Node in the list

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 class Node {
   constructor(val){
     this.val = val;
@@ -9,21 +7,29 @@ class Node {
 
 class SinglyLinkedList{
   constructor(){
-    this.head = head;
-    this.tail = tail;
+    this.head = null;
+    this.tail = null;
     this.howLong = 0;
   }
-  push(val) {
-    var newNode = new Node(val);
-
   // this function should accept a val
+  push(val) {
+    //create variable to store val in a Node
+    var newNode = new Node(val);
+  //If nothing in this.head
   if (!this.head) {
+    //this.head is now the new node
     this.head = newNode;
-    this.tail = this.head;
+    //this.tail is now head so  tail is null 
+    //we are saving every value that is saved 
+    this.tail= this.head;
+    //else meaning if we have a value in the this.head
   } else {
+    //the tail is now the new node
     this.tail.next = newNode;
+    //will be the node before
     this.tail = newNode;
     }
+    //we need to update the length
     this.howLong++
     return this;
   // create a new node using the value passed to the function
@@ -31,7 +37,42 @@ class SinglyLinkedList{
   // Otherwise set the next property on the tail to be the new node and set the tail propert on the list to be the newly created node
   //   Increment the length by one
   }
+    pop(){
+    //Edge Case if no nodes in list return undefiend
+    if (this.howLong === 0) {
+      return "empty";
+    }
+    //We need 2 variables both will start at this.head
+    var current = this.head;
+    var newTail = current;
+    while(current.next) {
+      //While there is a next this.tail will go up 
+      newTail = current;
+      //Current willl go to that next one
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    console.log(`The value of newTail is ${newTail.val}`)
+    console.log(`The value of Current is ${current.val}`)
+    this.howLong--;
+    if(this.howLong === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+    //Loop thru until you reach tail
+    //Set property of the 2nd to last to be null
+    //Set the tail to be the 2nd to last Node
+    //Decrement the length of the list by 1
+    //Return thye value of the node removed
+  }
 }
 var list = new SinglyLinkedList()
-list.push("HELLO")
-console.log(list);
+
+list.push("Hey");
+list.push("There");
+list.push("My");
+
+
+list.pop();
